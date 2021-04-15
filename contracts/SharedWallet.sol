13e172a362df44684097fcaaa91403e46b42c613
Poohdish R <poohdish@hey.com>
2021-04-15 23:40:23 +0700

    Require owner of within allowance. See line 24 on SharedWallet.sol

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -2,20 +2,27 @@
 
 pragma solidity ^0.8.1;
 
-contract SharedWallet {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    address owner;
+contract SharedWallet is Ownable {
 
-    constructor() {
-        owner = msg.sender;
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not allowed");
+    mapping(address => uint) public allowance;
+
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed!");
         _;
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+
         _to.transfer(_amount);
     }
 
