513b8161bc8b290056384e2b3bbdc76c98bf81b3
Poohdish R <poohdish@hey.com>
2021-04-15 23:46:26 +0700

    Extract allowance-related into its own contract.

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -4,15 +4,14 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
-
+contract Allowance is Ownable {
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
 
     mapping(address => uint) public allowance;
 
-    function addAllowance(address _who, uint _amount) public onlyOwner {
+    function setAllowance(address _who, uint _amount) public onlyOwner {
         allowance[_who] = _amount;
     }
 
@@ -21,8 +20,19 @@ contract SharedWallet is Ownable {
         _;
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+    function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        allowance[_who] -= _amount;
+    }
 
+}
+
+contract SharedWallet is Allowance {
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Contract doesn't own enough money");
+        if(!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
         _to.transfer(_amount);
     }
 
